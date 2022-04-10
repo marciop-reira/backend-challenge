614cc4dfa567b7840c63ef31c91277dcad0b765b
Márcio Pereira <marciop.usa@gmail.com>
2022-04-10 15:30:52 -0300

    Configure api routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/', fn() => "Back-end Challenge 2021 🏅 - Space Flight News");
+Route::apiResource('articles', ArticleController::class);
