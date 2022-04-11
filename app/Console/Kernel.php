0104dfc041583d9e97c9eb7a85fa3a0c58bd6da0
Márcio Pereira <marciop.usa@gmail.com>
2022-04-10 21:59:12 -0300

    Configure schedule of command update:articles

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,11 +5,12 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
+/**
+ *
+ */
 class Kernel extends ConsoleKernel
 {
     /**
-     * The Artisan commands provided by your application.
-     *
      * @var array
      */
     protected $commands = [
@@ -17,19 +18,15 @@ class Kernel extends ConsoleKernel
     ];
 
     /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+         $schedule->command('update:articles')->dailyAt('09:00');
     }
 
     /**
-     * Register the commands for the application.
-     *
      * @return void
      */
     protected function commands()
